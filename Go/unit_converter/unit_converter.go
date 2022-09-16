b4bc36bfdda6dc63d8332a6c8403d7ee7f5de6e4
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-09-16 15:34:24 +0400

    feat: init cli

--- Go/unit_converter/unit_converter.go
+++ Go/unit_converter/unit_converter.go
@@ -1,7 +1,47 @@
 package main
 
-import "github.com/MrTanoshii/effective-octo-train/Go/unit_converter/lib"
+import (
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/MrTanoshii/effective-octo-train/Go/unit_converter/lib"
+)
 
 func main() {
-	lib.UnitToConversionRate("m", "km")
+	var command string
+	var value float64
+
+	// Flags declaration
+	flag.StringVar(&command, "c", "length m mi", "Specify command. Default is length meter to miles.")
+	flag.Float64Var(&value, "v", 1, "Specify value. Default is 1.")
+
+	flag.Parse()
+
+	fmt.Println("~~ Unit Converter ~~")
+	fmt.Println()
+
+	fmt.Println("Received command:", command, "with values: ", value)
+	fmt.Println()
+
+	// Accept only 3 arguments in the command
+	commandArray := strings.Split(command, " ")
+	if len(commandArray) != 3 {
+		return
+	}
+
+	switch commandArray[0] {
+	case "length":
+		fmt.Println("Converter check: [Found]")
+	default:
+		fmt.Println("Invalid command. Please try again.")
+		return
+	}
+
+	var conversionRate float64 = lib.UnitToConversionRate(commandArray[1], commandArray[2])
+	if conversionRate == 0.0 {
+		return
+	} else {
+		fmt.Println("Conversion rate from", commandArray[1], "to", commandArray[2], "is", conversionRate)
+	}
 }

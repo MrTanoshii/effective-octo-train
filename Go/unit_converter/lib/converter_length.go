df4b31594db454d7ad21955989c45655b2291091
MrTanoshii <47116127+MrTanoshii@users.noreply.github.com>
2022-09-16 15:50:26 +0400

    refactor: remove prints

--- Go/unit_converter/lib/converter_length.go
+++ Go/unit_converter/lib/converter_length.go
@@ -1,12 +1,7 @@
 package lib
 
-import (
-	"fmt"
-)
-
 func LengthConverter(value float64, conversion_rate float64) float64 {
 	result := value * conversion_rate
 
-	fmt.Printf("Value: %v -> %v", value, result)
 	return result
 }
